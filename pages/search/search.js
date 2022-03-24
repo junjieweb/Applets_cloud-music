2cbbc30d59cb1f8c8f68cbeb6b62ddd968408763
junjieweb <297901657@qq.com>
2022-03-25 01:20:50 +0800

    update

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,5 +1,6 @@
 // pages/search/search.js
 import request from '../../utils/request'
+let isSend = false // 用于函数节流
 Page({
 
     /**
@@ -8,6 +9,9 @@ Page({
     data: {
         placeholderContent: '', // placeholder的内容
         hotList: [], // 热搜榜数据
+        searchContent: '', // 用户输入的表单项数据
+        searchList: [], // 关键字模糊匹配的数据
+        historyList: [] // 搜索历史记录
     },
 
     /**
@@ -16,6 +20,8 @@ Page({
     onLoad: function (options) {
         // 获取初始化数据
         this.getInitData()
+        // 获取历史记录
+        this.getSearchHistory();
     },
     // 获取初始化的数据
     async getInitData() {
@@ -25,6 +31,79 @@ Page({
             placeholderContent: placeholderData.data.showKeyword,
             hotList: hotListData.data
         })
+    },
+    // 表单项内容发生改变的回调
+    handleInputChange(event) {
+        // 更新searchContent状态数据
+        this.setData({
+            searchContent: event.detail.value.trim()
+        })
+        // 函数节流
+        if (isSend) {
+            return
+        }
+        isSend = true
+        this.getSearchList()
+        setTimeout(async () => {
+            isSend = false
+        }, 3000)
+    },
+    // 获取搜索数据的功能函数
+    async getSearchList() {
+        if (!this.data.searchContent) {
+            this.setData({
+                searchList: []
+            })
+            return
+        }
+        let { searchContent, historyList } = this.data
+        // 发请求获取关键字模糊匹配数据
+        let searchListData = await request('/search', { keywords: searchContent, limit: 10 })
+        this.setData({
+            searchList: searchListData.result.songs
+        })
+        // 将搜索的关键字添加到搜索历史记录中
+        if (historyList.indexOf(searchContent) !== -1) {
+            historyList.splice(historyList.indexOf(searchContent), 1);
+        }
+        historyList.unshift(searchContent);
+        this.setData({
+            historyList,
+        });
+        wx.setStorageSync("searchHistory", historyList);
+    },
+    // 获取本地历史记录的功能函数
+    getSearchHistory() {
+        let historyList = wx.getStorageSync("searchHistory");
+        if (historyList) {
+            this.setData({
+                historyList,
+            });
+        }
+    },
+    // 清空搜索内容
+    clearSearchContent() {
+        this.setData({
+            searchContent: "",
+            searchList: [],
+        });
+    },
+
+    // 删除搜索历史记录
+    deleteSearchHistory() {
+        wx.showModal({
+            content: "确认删除吗?",
+            success: (res) => {
+                if (res.confirm) {
+                    // 清空data中historyList
+                    this.setData({
+                        historyList: [],
+                    });
+                    // 移除本地的历史记录缓存
+                    wx.removeStorageSync("searchHistory");
+                }
+            },
+        });
     },
 
     /**
