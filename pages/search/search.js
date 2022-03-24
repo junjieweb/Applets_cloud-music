2d01acb446ade48715caee2faa1a1bb18a74d0ed
junjieweb <297901657@qq.com>
2022-03-25 00:02:48 +0800

    add commit

---
+++ pages/search/search.js
@@ -0,0 +1,78 @@
+// pages/search/search.js
+import request from '../../utils/request'
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        placeholderContent: '', // placeholder的内容
+        hotList: [], // 热搜榜数据
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        // 获取初始化数据
+        this.getInitData()
+    },
+    // 获取初始化的数据
+    async getInitData() {
+        let placeholderData = await request('/search/default')
+        let hotListData = await request('/search/hot/detail')
+        this.setData({
+            placeholderContent: placeholderData.data.showKeyword,
+            hotList: hotListData.data
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
